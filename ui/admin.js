77f3f7f3cdfb2d26429b87eff4f5821c3d24e042
Brice Hessou <bricehessou@gmail.com>
2023-01-08 19:37:52 +0100

    feat: Refresh auto

--- ui/admin.js
+++ ui/admin.js
@@ -1,2 +1,8 @@
 import '@/assets/scss/admin.scss'
 import '@/stimulus/bootstrap'
+
+document.addEventListener("DOMContentLoaded", function () {
+    setInterval(() => {
+        window.location.reload()
+    }, 45000)
+})
