76f0b7d6b9efc75c2d7f4490a99875b4b736a127
Brice Hessou <bricehessou@gmail.com>
2023-01-09 12:29:34 +0100

    feat: Activer le rafraichissement uniquement sur le tableau de bord.

--- ui/admin.js
+++ ui/admin.js
@@ -1,8 +1,3 @@
 import '@/assets/scss/admin.scss'
 import '@/stimulus/bootstrap'
 
-document.addEventListener("DOMContentLoaded", function () {
-    setInterval(() => {
-        window.location.reload()
-    }, 45000)
-})
